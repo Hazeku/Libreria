68ff6430468699d48dcb3c79e43671bde7aa7b66
franco <francolox13@gmail.com>
2025-05-19 17:57:55 -0300

    cambios on config. js para el render y en main go en cors allow origins la dirrecion del dominio de netlify para que pueda leerlos

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,3 @@
-//const API_URL = process.env.REACT_APP_API_URL;//
-const API_URL = 'https://libreria-mpu1.onrender.com';
+//const API_URL = process.env.REACT_APP_API_URL , 'https://libreria-mpu1.onrender.com'; //
+const API_URL = process.env.REACT_APP_API_URL;
 export default API_URL;
