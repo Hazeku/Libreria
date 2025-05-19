e283916b76e9704ebe44f112764681211ea3ad04
franco <francolox13@gmail.com>
2025-05-19 17:17:38 -0300

    cambios on config. js para el render y en main go en cors allow origins la dirrecion del dominio de netlify para que pueda leerlos

--- backend-go/main.go
+++ backend-go/main.go
@@ -64,7 +64,7 @@ func main() {
 
 	// CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://192.168.1.40:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://192.168.1.40:3000", "https://libreriaosvaldo.netlify.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL;
+//const API_URL = process.env.REACT_APP_API_URL;//
+const API_URL = 'https://libreria-mpu1.onrender.com';
 export default API_URL;
